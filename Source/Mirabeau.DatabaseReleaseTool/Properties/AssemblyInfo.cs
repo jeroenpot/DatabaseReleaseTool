c002309ce5076d0c98ed7ca7d9c572e6ee1291d1
Sander Kinds <skinds@mirabeau.nl>
2015-02-26 16:12:05 +0100

    added msbuild-by-convention

--- Source/Mirabeau.DatabaseReleaseTool/Properties/AssemblyInfo.cs
+++ Source/Mirabeau.DatabaseReleaseTool/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
 
@@ -10,10 +10,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mirabeau BV")]
 [assembly: AssemblyProduct("Mirabeau.DatabaseReleaseTool")]
-[assembly: AssemblyCopyright("Copyright � Mirabeau BV 2010")]
+[assembly: AssemblyCopyright("Copyright © Mirabeau BV 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: CLSCompliant(true)]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -24,12 +23,15 @@ using System.Runtime.InteropServices;
 [assembly: Guid("22cb3f69-47d4-423a-9dfd-d258cac16268")]
 
 // Version information for an assembly consists of the following four values:
+//
 //      Major Version
 //      Minor Version 
 //      Build Number
 //      Revision
+//
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.2.0")]
-[assembly: AssemblyFileVersion("1.2.2.0")]
+// [assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: CLSCompliant(true)]
